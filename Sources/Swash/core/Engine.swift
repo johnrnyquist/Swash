1bd34558cc441553c918e07321440dfa8160e6ab
johnrnyquist <john@nyquist.net>
2024-01-05 17:54:25 -0700

    updateComplete needs to be public so you can listen for when the update loop is complete.

--- Sources/Swash/core/Engine.swift
+++ Sources/Swash/core/Engine.swift
@@ -26,7 +26,7 @@ public class Engine {
     engine it is usually best not to do so during the update loop. To avoid this you can
     listen for this signal and make the change when the signal is dispatched.
     */
-    var updateComplete: Signaler0 = Signaler0()
+    public var updateComplete: Signaler0 = Signaler0()
     lazy private var componentAddedListener: Listener = { Listener(componentAdded) }()
     lazy private var componentRemovedListener: Listener = { Listener(componentRemoved) }()
     lazy private var entityNameChangedListener: Listener = { Listener(entityNameChanged) }()
