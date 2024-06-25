fa15bc214cfb3b816ced87ff02f7d29ea65f0227
johnrnyquist <john@nyquist.net>
2024-06-25 16:30:37 -0600

    FIX: very important fix, releaseNodePoolCache did not have the right signature to be listening to the Engine's updateComplete Signaler1. Changed the private entities dictionary to use entity names as keys instead of entities.

--- Sources/Swash/core/ComponentMatchingFamily.swift
+++ Sources/Swash/core/ComponentMatchingFamily.swift
@@ -1,19 +1,31 @@
 /**
+The default class for managing a NodeList. 
+ 
 This class adds and removes nodes to/from a node list as the entities and the components 
-in the engine change.  An `Entity` is added to this family’s node list if it contains components 
-matching all component class names in the components dictionary of the node subclass. 
+in the engine change.  
+ 
+`Entity`s are added to this family’s node list if it contains components 
+matching all component class names in the Node’s `components` dictionary. 
+ 
 The Engine in turn has a list of `ComponentMatchingFamily`s.
 */
+ 
+import Foundation
+
 open class ComponentMatchingFamily: IFamily, CustomStringConvertible {
-    public var description: String { "\(Self.self)_" + "\(nodeClassType)" }
+    /// The NodeList managed by this family.
+    /// This is a reference that always remains valid since it is retained and reused by Systems that use the list. 
+    /// i.e. we never recreate the list, we always modify it in place.
+    public private(set) var nodeList: NodeList
     private var componentClassNames: [ComponentClassName] = []
-    private var engine: Engine
-    private var entities: [Entity: Node] = [:]
-    private var nodePool: NodePool
+    private var entities: [String: Node] = [:]
     private var nodeClassType: Node.Type
+    private var nodePool: NodePool
+    private weak var engine: Engine?
+    public var description: String { "\(Self.self)_" + "\(nodeClassType)" }
+
     /**
-     Creates a ComponentMatchingFamily to provide a NodeList for the
-     given `Node` subclass.
+     Creates a ComponentMatchingFamily to provide a NodeList for the given `Node` subclass.
      - Parameters:
       - nodeClassType: The type for the node subclass.
       - engine: The engine for which this family is managing its node list.
@@ -26,12 +38,12 @@ open class ComponentMatchingFamily: IFamily, CustomStringConvertible {
         for (componentClassName, _) in nodeClassType.init().components {
             componentClassNames.append(componentClassName) //component.value
         }
+        print("Creating \(self) for \(nodeClassType)")
     }
 
-    /**
-    The nodelist managed by this family. This is a reference that remains valid always since it is retained and reused by Systems that use the list. i.e. we never recreate the list, we always modify it in place.
-    */
-    public var nodeList: NodeList
+    deinit {
+        engine = nil
+    }
 
     /**
      Called by the engine when an entity has been added to it.
@@ -70,7 +82,7 @@ open class ComponentMatchingFamily: IFamily, CustomStringConvertible {
     */
     func addIfMatch(entity: Entity) {
         // does the ComponentMatchingFamily have a reference to this entity yet?
-        guard entities[entity] == nil else { return }
+        guard entities[entity.name] == nil else { return }
         // go through all the components return if it does not have a component required by this family. 
         for componentClassName in componentClassNames {
             if entity.has(componentClassName: componentClassName) == false { return }
@@ -83,7 +95,7 @@ open class ComponentMatchingFamily: IFamily, CustomStringConvertible {
         for componentClassName in componentClassNames {
             node.components[componentClassName] = entity[componentClassName]
         }
-        entities[entity] = node
+        entities[entity.name] = node
         nodeList.add(node: node)
     }
 
@@ -91,11 +103,10 @@ open class ComponentMatchingFamily: IFamily, CustomStringConvertible {
     Removes the entity if it is in this family's NodeList.
     */
     func removeIfMatch(entity: Entity) {
-        guard let _ = entities[entity],
-              let node = entities[entity] else { return }
-        entities.removeValue(forKey: entity)
+        guard let node = entities[entity.name] else { return }
+        entities.removeValue(forKey: entity.name)
         nodeList.remove(node: node)
-        if (engine.updating) {
+        if let engine, engine.updating {
             nodePool.cache(node: node)
             engine.updateComplete.add(Listener(releaseNodePoolCache))
         } else {
@@ -107,8 +118,8 @@ open class ComponentMatchingFamily: IFamily, CustomStringConvertible {
      Releases the nodes that were added to the node pool during this engine update, 
      so they can be reused.
      */
-    private func releaseNodePoolCache() {
-        engine.updateComplete.remove(Listener(releaseNodePoolCache))
+    private func releaseNodePoolCache(time: TimeInterval) {
+        engine?.updateComplete.remove(Listener(releaseNodePoolCache))
         nodePool.releaseCache()
     }
 
@@ -119,39 +130,10 @@ open class ComponentMatchingFamily: IFamily, CustomStringConvertible {
         var node: Node? = nodeList.head
         while let currentNode = node {
             if let entity = currentNode.entity {
-                entities.removeValue(forKey: entity)
+                entities.removeValue(forKey: entity.name)
             }
             node = currentNode.next //end
         }
         nodeList.removeAll()
-    }
-
-    // MARK: - Deprecated
-    @available(iOS,
-               deprecated,
-               message: "The function `newEntity(entity:)` is deprecated and will be removed in version 1.1. Please use `new(entity:)` instead.")
-    public func newEntity(entity: Entity) {
-        new(entity: entity)
-    }
-
-    @available(iOS,
-               deprecated,
-               message: "The function `componentAddedToEntity(entity:)` is deprecated and will be removed in version 1.1. Please use `componentAddedTo(entity:)` instead.")
-    public func componentAddedToEntity(entity: Entity) {
-        componentAddedTo(entity: entity)
-    }
-
-    @available(iOS,
-               deprecated,
-               message: "The function `componentRemovedFromEntity(entity:)` is deprecated and will be removed in version 1.1. Please use `componentRemovedFrom(entity:)` instead.")
-    public func componentRemovedFromEntity(entity: Entity, componentClassName: ComponentClassName) {
-        componentRemovedFrom(entity: entity, componentClassName: componentClassName)
-    }
-
-    @available(iOS,
-               deprecated,
-               message: "The function `removeEntity(entity:)` is deprecated and will be removed in version 1.1. Please use `remove(entity:)` instead.")
-    public func removeEntity(entity: Entity) {
-        remove(entity: entity)
     }
 }
